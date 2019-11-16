7b30fb0b2e1bde15bb02c1bfd93f3ef106e21c89
Calum Flanagan <flanagancalum@gmail.com>
2019-11-16 17:46:19 +0000

    Revert "Add error handling on mongoose connection"
    This reverts commit 7a27822296b58b10fd773f88f02fe2402ebee718.

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongooose
-  .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
-  .catch(error => handleError(error));
+mongooose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongooose.connection;
 connection.once('open', () => {
